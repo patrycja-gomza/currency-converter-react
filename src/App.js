c1af7f1cbec477cdfbd698c8d455c97bec743206
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-10-11 12:23:03 +0200

    'currencies' array added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Container from "./Container";
 import Form from "./Form";
 import Result from "./Result";
-import Info from "./Info"
+import Info from "./Info";
+import currencies from "./currencies";
 
 function App() {
 
---
+++ src/currencies.js
@@ -0,0 +1,17 @@
+const currencies = [
+    {
+        description: 'frank szwajcarski',
+        name: 'CHF',
+        rate: 4.63,
+    },
+    {
+        description: 'euro',
+        name: 'EUR',
+        rate: 4.66,
+    },
+    {
+        description: 'dolar amerykański',
+        name: 'USD',
+        rate: 4.15,
+    },
+];
