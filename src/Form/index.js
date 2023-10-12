5797b0a53cbbcaeb793fb400629819afca2cd52d
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-10-12 15:03:39 +0200

    adding controlled components to input and select, declaring new state variables for 'currency' and 'amount', calling preventDefault() on submit event

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,31 +1,62 @@
 import "./style.css";
+import currencies from "../currencies";
+import { useState } from "react";
 
-const Form = () => (
-    <form>
-        <fieldset className="form__fieldset">
-            <legend className="form__legend">Kalkulator walut</legend>
-            <p>
-                <label>
-                    <span className="form__label"> Wybierz walutę </span>
-                    <select className="form__field" name="currency">
-                        <option value="CHF">CHF</option>
-                        <option value="EUR">EUR</option>
-                        <option value="USD">USD</option>
-                    </select>
-                </label>
+const Form = () => {
+    const [currency, setCurrency] = useState("");
+    const [amount, setAmount] = useState("");
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <form onSubmit={onFormSubmit}>
+            <fieldset className="form__fieldset">
+                <legend className="form__legend">Kalkulator walut</legend>
+
+                <p>
+                    <label>
+                        <span className="form__label"> Wybierz walutę </span>
+                        <select
+                            className="form__field"
+                            onChange={(event) => setCurrency(event.target.value)}
+                        >
+                            {currencies.map((currency) => (
+                                <option
+                                    key={currency.description}
+                                    value={currency.name}
+                                >
+                                    {currency.name}
+                                </option>
+                            ))}
+                        </select>
+                    </label>
+                </p>
+
+                <p>
+                    <label>
+                        <span className="form__label"> Wpisz kwotę * </span>
+                        <input
+                            className="form__field"
+                            required type="number"
+                            min="1"
+                            step="any"
+                            placeholder="Wpisz kwotę w wybranej przez siebie walucie"
+                            value={amount}
+                            onChange={(event) => setAmount(event.target.value)}
+                        />
+                    </label>
+                </p>
+            </fieldset>
+
+            <p className="form__container">
+                <button className="form__button">
+                    Przelicz!
+                </button>
             </p>
-            <p>
-                <label>
-                    <span className="form__label"> Wpisz kwotę * </span>
-                    <input className="form__field" required type="number" min="1" step="any"
-                        placeholder="Wpisz kwotę w wybranej przez siebie walucie" />
-                </label>
-            </p>
-        </fieldset>
-        <p className="form__container">
-            <button className="form__button">Przelicz!</button>
-        </p>
-    </form>
-);
+        </form>
+    );
+};
 
 export default Form;
