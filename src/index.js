76263efa4235320f59c8d8a812dba5450ea559aa
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-10-28 13:28:08 +0200

    organizing files in the project

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { GlobalStyle } from "./GlobalStyle";
 import { ThemeProvider } from 'styled-components';
 import { theme } from "./theme";
-import App from './App';
+import App from './App/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
