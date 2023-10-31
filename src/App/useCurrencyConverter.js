10333cc64b1a83410c214ba70830dc4d7fc9424f
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-10-31 13:35:55 +0100

    adding async functions for getting data from JSON file and from API

--- src/App/useCurrencyConverter.js
+++ src/App/useCurrencyConverter.js
@@ -1,1 +1,22 @@
 import axios from 'axios';
+
+const takeRatesFromJSON = (async () => {
+    try {
+        const response = await axios.get("currencyRates.json");
+        return response.data;
+    } catch (error) {
+        console.error("Błąd podczas pobierania danych z pliku JSON", error);
+    }
+})();
+
+const takeRatesFromAPI = (async () => {
+    try {
+        const response = await axios.get("https://api.currencyapi.com/v3/latest?apikey=cur_live_KQwImW9dizpQDU9CU4gRlOol4ox1vumKqwQWNRq1");
+        return {
+            rates: response.data.rates,
+            date: response.data.date,
+        };
+    } catch (error) {
+        console.error("Błąd podczas pobierania kursów walut z API", error);
+    }
+})();
