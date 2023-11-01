8bc1b23f7d435e189040534ebfaad8ad4fb5c6e8
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-11-01 12:26:33 +0100

    creating a state variable 'ratesData'; combining the two functions 'takeRatesFrom...' into one with a selectable url for taking data

--- src/App/useCurrencyConverter.js
+++ src/App/useCurrencyConverter.js
@@ -1,22 +1,21 @@
 import axios from 'axios';
+import { useState, useEffect } from "react";
 
-const takeRatesFromJSON = (async () => {
-    try {
-        const response = await axios.get("currencyRates.json");
-        return response.data;
-    } catch (error) {
-        console.error("Błąd podczas pobierania danych z pliku JSON", error);
-    }
-})();
+export const useCurrencyConverter = () => {
+    const [ratesData, setRatesData] = useState();
 
-const takeRatesFromAPI = (async () => {
-    try {
-        const response = await axios.get("https://api.currencyapi.com/v3/latest?apikey=cur_live_KQwImW9dizpQDU9CU4gRlOol4ox1vumKqwQWNRq1");
-        return {
-            rates: response.data.rates,
-            date: response.data.date,
-        };
-    } catch (error) {
-        console.error("Błąd podczas pobierania kursów walut z API", error);
-    }
-})();
+    const urlJson = "https://raw.githubusercontent.com/patrycja-gomza/currency-converter-react/main/src/App/currencyRates.json";
+    //const urlApi = "https://api.currencyapi.com/v3/latest?apikey=cur_live_KQwImW9dizpQDU9CU4gRlOol4ox1vumKqwQWNRq1&currencies=EUR%2CUSD%2CCAD&base_currency=PLN";
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await axios.get(urlJson);
+                setRatesData(response.data);
+            } catch (error) {
+                setRatesData({ error: true });
+            }
+        })();
+    }, []);
+    return ratesData;
+};
