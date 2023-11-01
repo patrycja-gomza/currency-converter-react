6e1375ecb5456aad7003080c5607de5d3856879f
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-11-01 14:42:33 +0100

    adding setTimeout

--- src/App/useCurrencyConverter.js
+++ src/App/useCurrencyConverter.js
@@ -8,14 +8,17 @@ export const useCurrencyConverter = () => {
     //const urlApi = "https://api.currencyapi.com/v3/latest?apikey=cur_live_KQwImW9dizpQDU9CU4gRlOol4ox1vumKqwQWNRq1&currencies=EUR%2CUSD%2CCAD&base_currency=PLN";
 
     useEffect(() => {
-        (async () => {
-            try {
-                const response = await axios.get(urlJson);
-                setRatesData(response.data);
-            } catch (error) {
-                setRatesData({ error: true });
-            }
-        })();
+        setTimeout(() => {
+            (async () => {
+                try {
+                    const response = await axios.get(urlJson);
+                    setRatesData({ ...response.data });
+                } catch (error) {
+                    setRatesData({ error: true });
+                }
+            })();
+        }, 5000);
     }, []);
+
     return ratesData;
 };
