e14bb97c15b823e8851ce415ae926bfec96a1b82
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-11-04 11:41:11 +0100

    moving the url above the hook and renaming to 'API_URL' 'JSON_URL' as recommended by the mentor

--- src/App/useRatesData.js
+++ src/App/useRatesData.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 import { useState, useEffect } from "react";
 
+//const JSON_URL = "https://raw.githubusercontent.com/patrycja-gomza/currency-converter-react/main/src/App/currencyRates.json";
+const API_URL = "https://api.currencyapi.com/v3/latest?apikey=cur_live_KQwImW9dizpQDU9CU4gRlOol4ox1vumKqwQWNRq1&currencies=EUR%2CUSD%2CCAD&base_currency=PLN";
+
 export const useRatesData = () => {
     const [ratesData, setRatesData] = useState(
         { loading: true }
     );
 
-    //const urlJson = "https://raw.githubusercontent.com/patrycja-gomza/currency-converter-react/main/src/App/currencyRates.json";
-    const urlApi = "https://api.currencyapi.com/v3/latest?apikey=cur_live_KQwImW9dizpQDU9CU4gRlOol4ox1vumKqwQWNRq1&currencies=EUR%2CUSD%2CCAD&base_currency=PLN";
-
     useEffect(() => {
         setTimeout(() => {
             (async () => {
                 try {
-                    const response = await axios.get(urlApi);
+                    const response = await axios.get(API_URL);
                     setRatesData({ ...response.data });
                 } catch (error) {
                     setRatesData({ error: true });
