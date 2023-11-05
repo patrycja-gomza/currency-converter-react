e878344b0d26695c95f1c50a4ab9959fba32e171
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-11-05 16:02:58 +0100

    replacing ternary operator with early return

--- src/App/LoadingAndError/index.js
+++ src/App/LoadingAndError/index.js
@@ -2,32 +2,28 @@ import { Header, Message } from "./styled";
 
 const LoadingAndError = ({ ratesData, children }) => {
 
+    if (ratesData.status !== "loading" && ratesData.status !== "error") {
+        return children;
+    }
+
     return (
         <>
-            {(ratesData.status === "loading" || ratesData.status === "error") ? (
-                <>
-                    <Header>Kalkulator walut</Header>
+            <Header>Kalkulator walut</Header>
 
-                    {ratesData.status === "loading" ? (
-                        <Message $loading>
-                            Sekundka...
-                            <br />
-                            Ładuję kursy walut z Europejskiego Banku Centralnego 😎
-                        </Message>
-                    ) : ratesData.status === "error" ? (
-                        <Message $error>
-                            Hmm... Coś poszło nie tak ☹️ Sprawdź, czy masz połączenie z internetem.
-                            <br />
-                            Jeśli masz... to wygląda na to, że to nasza wina.
-                            <br />
-                            Może spróbuj później? 😉
-                        </Message>
-                    ) : null}
-                </>
+            {ratesData.status === "loading" ? (
+                <Message $loading>
+                    Sekundka...
+                    <br />
+                    Ładuję kursy walut z Europejskiego Banku Centralnego 😎
+                </Message>
             ) : (
-                <>
-                    {children}
-                </>
+                <Message $error>
+                    Hmm... Coś poszło nie tak ☹️ Sprawdź, czy masz połączenie z internetem.
+                    <br />
+                    Jeśli masz... to wygląda na to, że to nasza wina.
+                    <br />
+                    Może spróbuj później? 😉
+                </Message>
             )}
         </>
     );
