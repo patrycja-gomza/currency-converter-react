99348da505a6403c9387cd5215421f3bedfad817
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-11-14 19:57:32 +0100

    change api url

--- src/App/useRatesData.js
+++ src/App/useRatesData.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useState, useEffect } from "react";
 
 //const JSON_URL = "https://raw.githubusercontent.com/patrycja-gomza/currency-converter-react/main/src/App/currencyRates.json";
-const API_URL = "https://api.currencyapi.com/v3/latest?apikey=cur_live_KQwImW9dizpQDU9CU4gRlOol4ox1vumKqwQWNRq1&&base_currency=PLN";
+const API_URL = "https://api.currencyapi.com/v3/latest?apikey=cur_live_KQwImW9dizpQDU9CU4gRlOol4ox1vumKqwQWNRq1&currencies=&base_currency=PLN";
 
 export const useRatesData = () => {
     const [ratesData, setRatesData] = useState(
