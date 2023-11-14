09f4e0cfcd1b085b2e85fc34fc6641b15c4f5155
Patrycja Gomza <patrycja.gomza@gmail.com>
2023-11-14 19:39:11 +0100

    replace json url with api url

--- src/App/useRatesData.js
+++ src/App/useRatesData.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { useState, useEffect } from "react";
 
-const JSON_URL = "https://raw.githubusercontent.com/patrycja-gomza/currency-converter-react/main/src/App/currencyRates.json";
-//const API_URL = "https://api.currencyapi.com/v3/latest?apikey=cur_live_KQwImW9dizpQDU9CU4gRlOol4ox1vumKqwQWNRq1&currencies=EUR%2CUSD%2CCAD&base_currency=PLN";
+//const JSON_URL = "https://raw.githubusercontent.com/patrycja-gomza/currency-converter-react/main/src/App/currencyRates.json";
+const API_URL = "https://api.currencyapi.com/v3/latest?apikey=cur_live_KQwImW9dizpQDU9CU4gRlOol4ox1vumKqwQWNRq1&&base_currency=PLN";
 
 export const useRatesData = () => {
     const [ratesData, setRatesData] = useState(
@@ -13,7 +13,7 @@ export const useRatesData = () => {
         setTimeout(() => {
             (async () => {
                 try {
-                    const response = await axios.get(JSON_URL);
+                    const response = await axios.get(API_URL);
                     setRatesData({
                         status: "success",
                         data: response.data,
